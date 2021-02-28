986ed09a75cdaa550e36a0f9a2150ae58edb2826
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-02-28 07:14:26 +0530

    replaced better wtforms

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from src import avail,all_trains,booking,all_booked,cancel
+from src import avail,all_trains,booking,all_booked,cancel,forms
 from flask import Flask
 from flask import render_template
 from flask import request, redirect
 
 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/form',methods=['GET','POST'])
+def login():
+    form  = forms.LoginForm()
+    if form.validate_on_submit():
+        return'<h1>The username is {}. The password is {}.'.format(form.username.data,form.password.data)
+    return render_template('login.html',form = form)
 
 @app.route('/all')
 def allTrains():
@@ -17,12 +28,11 @@ def allTrains():
 
 @app.route('/avail',methods=['POST','GET'])
 def available():
-    if request.method == 'POST': 
-        dat = request.form['date']
-        rows = avail.Avail(dat)
-        return render_template('avail.html',rows=rows)
-    else:
-        return render_template('avail.html')
+    form = forms.AvailForm()
+    if form.is_submitted():
+        rows = avail.Avail(form.date.data)
+        return render_template('avail.html',rows=rows,form=form)
+    return render_template('avail.html',form=form)
 
 @app.route('/all_booked')
 def booked():
@@ -31,13 +41,12 @@ def booked():
 
 @app.route('/book',methods = ['POST','GET'])
 def bookings():
-    if request.method == 'POST': 
-        tid = request.form['train_id']
-        count = int(request.form['count'] )      
-        rows = booking.book(tid,count)
-        return render_template('book.html',rows=rows,tot = count*rows[6])
+    form = forms.BookForm()
+    if form.is_submitted():     
+        rows = booking.book(form.train_id.data,int(form.count.data))
+        return render_template('bookings.html',rows=rows,tot = int(form.count.data)*rows[6],form = form)
     else:
-        return render_template('book.html')
+        return render_template('bookings.html',form = form)
 
 @app.route('/cancel')
 def cancelling():
@@ -46,5 +55,5 @@ def cancelling():
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True,port=8000)
 
---
+++ src/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,IntegerField
+from wtforms.validators import InputRequired
+
+class LoginForm(FlaskForm):
+    username= StringField('username',validators=[InputRequired()])
+    password= PasswordField('password',validators=[InputRequired()])
+
+class AvailForm(FlaskForm):
+    date = StringField('date')
+
+class BookForm(FlaskForm):
+    train_id = StringField('train_id',validators=[InputRequired()])
+    count = IntegerField('count',validators=[InputRequired()])
