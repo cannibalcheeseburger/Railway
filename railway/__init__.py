8dc660bb3093e3e38750b852180b5565efee9dba
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-03-17 08:28:28 +0530

    as

--- railway/__init__.py
+++ railway/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
 
 from railway import routes
--- railway/models.py
+++ railway/models.py
@@ -1,8 +1,13 @@
-from railway import db
+from railway import db,login_manager
+from flask_login import UserMixin
 
-class Users(db.Model):
-    uid = db.Column(db.String,primary_key = True)
-    password = db.Column(db.String,nullable = False)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Users(db.Model,UserMixin):
+    uid = db.Column(db.String(20),primary_key = True)
+    password = db.Column(db.String(20),nullable = False)
     balance = db.Column(db.Integer,default = 0)
 
 class Trains(db.Model):
@@ -11,6 +16,6 @@ class Trains(db.Model):
     balance = db.Column(db.Integer,default = 0)
 class Booking(db.Model):
     book_id = db.Column(db.Integer,primary_key = True)
-    uid = db.Column(db.String,nullable = False)
+    uid = db.Column(db.String(20),nullable = False)
     train_id = db.Column(db.Integer,nullable = False)
     num_booked = db.Column(db.Integer,nullable = False)
