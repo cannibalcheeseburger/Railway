740b83e5540aca99409c3f2075e01f84db9fbdfd
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-04-07 10:52:54 +0530

    ok

--- railway/trains/urls.py
+++ railway/trains/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import HomeTemplateView, AllTrainsListView,booked,TrainsDetailView
-from .views import search
+from .views import search,register_page,login_page
 
 
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path('all/',AllTrainsListView.as_view(),name = 'all_trains'),
     path('all_booked/',booked,name = 'all_booked'),
     path('trains/<int:pk>',TrainsDetailView.as_view(),name ='trains'),
-    path('search/',search,name='search')
+    path('search/',search,name='search'),
+    path('register/',register_page,name='register'),
+    path('login/',login_page,name='login')
 ]
--- railway/trains/views.py
+++ railway/trains/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import TemplateView
 from .filters import TrainsFilter
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from .models import Users,Trains,Booking
@@ -23,6 +24,21 @@ class TrainsDetailView(DetailView):
     template_name = 'train_details.html'
     context_object_name = 'train'
     
+def register_page(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request,'Register.html',context)
+
+def login_page(request):
+    context = {'asd':'asd'}
+    return render(request,'login.html',context)
+
 
 def booked(request):
     context = {'Bookings' : Booking.objects.filter(users='Kash')}
